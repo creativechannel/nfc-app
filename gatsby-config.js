8daeaad219d8a6522938b4566b6d971fbcf39c01
Mario Morales <marioalejandrom@gmail.com>
2021-01-27 11:53:33 -0600

    Added new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: "#6b37bf",
         theme_color: "#6b37bf",
         display: "standalone",
-        icon: "src/images/icon.png",
+        icon: "src/images/icon.jpg",
         crossOrigin: `use-credentials`
       }
     },
