84aa3b7c7c7690216f5aad698bc7e24428e286e6
Mario Morales <marioalejandrom@gmail.com>
2021-02-23 16:13:48 -0600

    retailer promos style fixes

--- src/components/RetailerBlock.js
+++ src/components/RetailerBlock.js
@@ -5,7 +5,7 @@ const RetailerBlock = () => {
   return (<div className={"d-flex flex-column"}>
     <h2 className={"text-center mb-5"}>Retailer Promotions</h2>
     <Row>
-      <Col xs={6}>
+      <Col xs={6} lg={3}>
         <div className={"d-flex justify-content-center align-items-center"} style={{
           height: "100px", borderRadius: 5,
           border: "1px solid #b2b2b2",
@@ -14,10 +14,10 @@ const RetailerBlock = () => {
           backgroundColor: "#fff",
           boxShadow: "0px 3px 4px 0px #0000004d"
         }}>
-          <img src={"/retailer_logos/best_buy.png"} alt={"Lowes"} className={"img-fluid"}/>
+          <img src={"/retailer_logos/best_buy.png"} alt={"Lowes"} className={"img-fluid"} style={{maxHeight:90}}/>
         </div>
       </Col>
-      <Col xs={6}>
+      <Col xs={6} lg={3}>
         <div className={"d-flex justify-content-center align-items-center"} style={{
           height: "100px", borderRadius: 5,
           border: "1px solid #b2b2b2",
@@ -26,10 +26,10 @@ const RetailerBlock = () => {
           backgroundColor: "#fff",
           boxShadow: "0px 3px 4px 0px #0000004d"
         }}>
-          <img src={"/retailer_logos/costco.png"} alt={"Lowes"} className={"img-fluid"}/>
+          <img src={"/retailer_logos/costco.png"} alt={"Lowes"} className={"img-fluid"} style={{maxHeight:90}}/>
         </div>
       </Col>
-      <Col xs={6}>
+      <Col xs={6} lg={3}>
         <div className={"d-flex justify-content-center align-items-center"} style={{
           height: "100px", borderRadius: 5,
           border: "1px solid #b2b2b2",
@@ -38,10 +38,10 @@ const RetailerBlock = () => {
           backgroundColor: "#fff",
           boxShadow: "0px 3px 4px 0px #0000004d"
         }}>
-          <img src={"/retailer_logos/lowes.png"} alt={"Lowes"} className={"img-fluid"}/>
+          <img src={"/retailer_logos/lowes.png"} alt={"Lowes"} className={"img-fluid"} style={{maxHeight:90}}/>
         </div>
       </Col>
-      <Col xs={6}>
+      <Col xs={6} lg={3}>
         <div className={"d-flex justify-content-center align-items-center"} style={{
           height: "100px", borderRadius: 5,
           border: "1px solid #b2b2b2",
@@ -50,7 +50,7 @@ const RetailerBlock = () => {
           backgroundColor: "#fff",
           boxShadow: "0px 3px 4px 0px #0000004d"
         }}>
-          <img src={"/retailer_logos/bed_bath_beyond.png"} alt={"Lowes"} className={"img-fluid"}/>
+          <img src={"/retailer_logos/bed_bath_beyond.png"} alt={"Lowes"} className={"img-fluid"} style={{maxHeight:90}}/>
         </div>
       </Col>
     </Row>
