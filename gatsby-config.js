2f4418e8566f1a0a59d745431ca7258cc5ca8433
Mario Morales <marioalejandrom@gmail.com>
2021-02-24 09:41:24 -0600

    Fixes for Gastby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const activeEnv =  "development"
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 console.log("Active Env:", process.env.GATSBY_ACTIVE_ENV)
 console.log(`Using environment config: '${activeEnv}'`)
 require("dotenv").config({
